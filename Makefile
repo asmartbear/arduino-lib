7384dd121bd8ced1dc3c43f759e7bc5881917065
Jason Cohen <esmartbear@gmail.com>
2019-03-03 14:20:03 -0600

    updated for installing library into Arduino land

--- Makefile
+++ Makefile
@@ -5,10 +5,10 @@ C_FILES := $(wildcard src/*.cpp)
 O_FILES := $(patsubst src/%.cpp, bin/%.o, $(C_FILES))
 TESTS := bin/trig_test
 
-.PHONY: clean test-trig
+.PHONY: clean test-trig installs
 	
 all: $(O_FILES) $(TESTS)
-	
+
 clean:
 	rm -f bin/*
 
@@ -21,3 +21,5 @@ bin/%.o: src/%.cpp src/arduino.h Makefile
 bin/trig_test: bin/trig.o
 	cc bin/trig.o -o bin/trig_test
 
+install: $(O_FILES)
+	cp src/status.cpp src/status.h ~/Dropbox/dev/Arduino/libraries/cohen
--- src/status.cpp
+++ src/status.cpp
@@ -1,12 +1,6 @@
+#include "status.h"
 
 long ms_last_status = 0;    // when did we last emit status?
-
-// Defined by the application: Reset your system status
-void status_reset();
-
-// Defined by the application: Display your status across the given time-interval.
-// Do not also reset status -- that will be called for you.
-void status_display(long ms_duration);
 
 // Call this as often as possible from your loop, supplying the current value of `millis()`.
 void status_loop(long ms) {
