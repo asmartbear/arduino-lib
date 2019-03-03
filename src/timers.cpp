7964ca02a8cdca079480195c9c701ac67ae047a1
Jason Cohen <esmartbear@gmail.com>
2019-03-03 15:43:49 -0600

    full timers object

--- src/timers.cpp
+++ src/timers.cpp
@@ -1,8 +1,44 @@
-#include <Arduino.h>
 #include "timers.h"
 
 CTimers Timers;
 
-int CTimers::hello() {
-	return 123;
+void CTimers::loop() {
+	unsigned long interval, last;
+	
+	// Load the current time
+	unsigned long ms = millis();
+	
+	// Look for slots which need to be triggered
+	int slot = MAX_NUM_TIMERS;
+	while (slot--) {
+		if (this->timers[slot].ms_interval) {		// valid slot?
+			interval = this->timers[slot].ms_interval;
+			last = this->timers[slot].ms_last_triggered;
+			if ((ms / interval) > (last / interval)) {		// have we entered a new interval?
+				this->timers[slot].callback(ms - last);
+				this->timers[slot].ms_last_triggered = ms;
+			}
+		}
+	}
 }
+
+int CTimers::create(unsigned long ms_interval, timer_cb cb) {
+	
+	// Find the next available slot
+	int slot = MAX_NUM_TIMERS;
+	while (slot--) {
+		if (this->timers[slot].ms_interval == 0) {
+			break;
+		}
+	}
+	if (slot < 0) {
+		return -1;
+	}
+	
+	// Fill the info of that slot and return the slot number
+	this->timers[slot].ms_interval = ms_interval;
+	this->timers[slot].ms_last_triggered = 0;
+	this->timers[slot].callback = cb;
+	return slot;
+}
+
--- src/timers.h
+++ src/timers.h
@@ -1,14 +1,31 @@
 /*
 	Subsystem in which you can register things to happen on certain time intervals, and they will be executed.
   The time intervals are based on wall-clock, and so will stay as aligned as possible to real time, not from
-  the last execution.
+  the last execution.  However, if more than one wall-clock interval has passed, it will be triggered only
+  the one time.
  */
+#include <Arduino.h>
 
+#define MAX_NUM_TIMERS	10
+
+typedef void (*timer_cb)(unsigned long ms_interval);
 
 class CTimers {
+private:
+	
+	struct {
+		unsigned long ms_last_triggered;
+		unsigned long ms_interval;
+		timer_cb callback;
+	} timers[MAX_NUM_TIMERS];
+	
 public:
 	
-	int hello();
+	// Call this function as often as possible, e.g. in the main loop.
+	void loop();
+	
+	// Register a new timer, calling the given function on the given millisecond interval
+	int create(unsigned long ms_interval, timer_cb cb);
 	
 };
 
